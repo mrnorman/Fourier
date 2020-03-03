c7d58ddccd4f66bd0a759f86cf7e87e884d6cf71
Matt Norman <normanmr@ornl.gov>
2020-03-02 23:01:44 -0500

    More efficient DFTs

--- dft.cpp
+++ dft.cpp
@@ -3,66 +3,65 @@
 #include <iomanip>
 #include <cmath>
 
+#define YAKL_INLINE inline
+
 
 template <unsigned N, class T=double> class DFT {
+  T cos_table[N];
+  T sin_table[N];
 public:
-  T sin_table[N*N];
-
-
-  constexpr DFT() {
-    for (unsigned i=0; i<N*N; i++) {
+  YAKL_INLINE constexpr DFT() {
+    for (unsigned i=0; i<N; i++) {
+      cos_table[i] = cos(2*M_PI*i/N);
       sin_table[i] = sin(2*M_PI*i/N);
     }
   }
 
+  static YAKL_INLINE unsigned constexpr wrap( unsigned ind ) {
+    return ind - (ind/N)*N;
+  }
 
-  inline void forward_one(T const *data, T *fft, unsigned l) const {
+  YAKL_INLINE void forward_one(T const *data, T *fft, unsigned l) const {
     fft[l] = 0;
     // The i-loop is sequential
     for (unsigned i=0; i<N; i++) {
-      int sgn = -1;
-      unsigned ind = i*(l/2);  // Defaults to sin(2*pi*i*(l/2)/N)
-      // Only do integer arithmetic in the branched section
+      unsigned ind = wrap(i*(l/2));
+      T trig;
       if (l - ( (l >> 1) << 1 ) == 0) { // This is a cheap l%2 operator
-        // if l is even, compute cos(2*pi*i*(l/2)/N) by shifting by pi/2
-        ind = i*(l/2) + N/4;
-        if (ind >= N*N) { ind -= N; }
-        sgn = 1;
+        trig = cos_table[ind];
+      } else {
+        trig = -sin_table[ind];
       }
-      fft[l] += sgn*data[i]*sin_table[ind];
+      fft[l] += trig*data[i];
     }
     fft[l] /= N;
   }
 
-
-  inline void inverse_one(T *data, T const *fft, unsigned l) const {
+  YAKL_INLINE void inverse_one(T *data, T const *fft, unsigned l) const {
     data[l] = 0;
     // The i-loop is sequential
     for (unsigned i=0; i<N; i++) {
-      T mysin = sin_table[i*l];
-      unsigned ind = i*l + N/4;
-      if (ind >= N*N) { ind -= N; }
-      T mycos = sin_table[ind];
-      // Only do integer arithmetic in the branched section
-      if (i <= N/2) {
-        ind = 2*i;
-        mysin *= -1;
-      } else {
-        ind = 2*(N-i);
+      unsigned ind_tab = wrap(i*l);
+      unsigned ind_fft = 2*i;
+      int      sgn     = -1;
+      if (i > N/2) {
+        ind_fft = 2*(N-i);
+        sgn     = 1;
       }
-      data[l] += fft[ind]*mycos + fft[ind+1]*mysin;
+      data[l] +=     fft[ind_fft  ]*cos_table[ind_tab] +
+                 sgn*fft[ind_fft+1]*sin_table[ind_tab];
     }
   }
 
 
-  inline void forward(T const *data, T *fft) const {
+  YAKL_INLINE void forward(T const *data, T *fft) const {
     for (unsigned l=0; l<N+2; l++) {
       forward_one(data, fft, l);
     }
   }
 
 
-  inline void inverse(T *data, T const *fft) const {
+  YAKL_INLINE void inverse(T *data, T const *fft) const {
     for (unsigned l=0; l<N; l++) {
       inverse_one(data, fft, l);
     }
@@ -76,7 +75,7 @@ public:
 
 int main() {
   {
-    unsigned constexpr N = 8;
+    unsigned constexpr N = 12;
     double data[N  ];
     double fft [N+2];
 
@@ -126,6 +125,9 @@ int main() {
     DFT<N> dft;
     double data[N  ];
     double ffts[N+2];
+    for (unsigned i=0; i<N; i++) {
+      data[i] = pow(i+14.2,2);
+    }
 
     auto t1 = std::clock();
 
